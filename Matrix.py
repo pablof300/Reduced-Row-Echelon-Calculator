8df1ae1b184279c36bc439bb8938da13785d81ed
pablof300 <pablof300@gmail.com>
2018-02-05 17:58:22 -0500

    Double-enter now finalizes typing the matrix

--- Matrix.py
+++ Matrix.py
@@ -33,8 +33,11 @@ class Matrix(object):
         pass
 
 
+# Main program methods
+
 def printSpacers():
-    print " " * 3
+    for i in range(0,3):
+        print " "
 
 def isValidRowString(rowString):
     if "," not in rowString:
@@ -47,6 +50,19 @@ def isValidRowString(rowString):
             return False
     return True
 
+def isEmpty(rowString):
+    if len(rowString) <= 0:
+        return True
+    return False;
+
+def getReducedRowEchelonMatrix():
+    print "TO BE ADDED"
+
+
+
+# Start the program
+# User will be asked to input a matrix
+
 printSpacers()
 print "Please type a rectangular matrix row by row."
 print "If the first row contains a [1, 2, 3] (left-to-right), then type 1,2,3"
@@ -54,12 +70,24 @@ print "Once you type the last line, press enter twice!"
 printSpacers()
 
 currentLine = 0;
+emptyLines = 0;
 lines = []
 while(True):
     line = raw_input("Row %d> " %(currentLine + 1))
+    if isEmpty(line):
+        
+        # The player has pressed double entered and finished typing his matrix
+        if emptyLines >= 1:
+            getReducedRowEchelonMatrix()
+            break
+        
+        emptyLines += 1
+        continue
+    
     if isValidRowString(line):
         lines.append(line)
         currentLine += 1
+        emptyLines = 0
     else:
         print "Please type row %d correctly, or double press enter to finish typing the matrix"
                      
