ea7686f9e3269a926d89a0f9bb98df59d5ee1de5
pablof300 <pablof300@gmail.com>
2018-02-17 10:59:53 -0500

    Converted most variables to use snake_case

--- Matrix.py
+++ Matrix.py
@@ -5,7 +5,7 @@ class Row(object):
         """Holds an array of integers that represent a row in a matrix"""
         self.values = values
         self.size = len(self.values)
-        self.isEmpty = self.isEmpty()
+        self.empty = self.isEmpty()
 
     def isEmpty(self):
         for value in self.values:
@@ -14,32 +14,33 @@ class Row(object):
         return True
 
 class Matrix(object):
+    """Represents a rectangular matrix to be solved using reduced row echelon"""
+    
     def __init__(self, lines):
-        """Represents a rectangular matrix to be solved using reduced row echelon"""
         self.rows = self.getRows(lines)
         self.toReducedRowEchelonMatrix()
 
     def toReducedRowEchelonMatrix(self):
         """Transforms the matrix into a matrix in reduced row echelon form"""
         self.normalize()
-        pivotDictionary = self.getPivotColDictionary()
-        if self.arePivotColumnsRepeated(pivotDictionary):
-            self.rows = self.getRowsWithUniquePivots(pivotDictionary)
+        pivot_dictionary = self.getPivotColDictionary()
+        if self.arePivotColumnsRepeated(pivot_dictionary):
+            self.rows = self.getRowsWithUniquePivots(pivot_dictionary)
             self.toReducedRowEchelonMatrix()
         else:
             self.fromRowEchelonToReducedRowEchelon()
 
     def getRows(self, lines):
         """Transforms an array of Strings (which hold the rows inputted by user) into Row objects"""
-        matrixRows = []
-        updatedLines = [newLine.replace(" ", "") for newLine in lines]
-        for line in updatedLines:
-            rowElements = []
+        matrix_rows = []
+        updated_lines = [newLine.replace(" ", "") for newLine in lines]
+        for line in updated_lines:
+            row_elements = []
             elements = line.split(",")
             for element in elements:
-                rowElements.append(int(element))
-            matrixRows.append(Row(rowElements))
-        return matrixRows
+                row_elements.append(int(element))
+            matrix_rows.append(Row(row_elements))
+        return matrix_rows
 
     def printMatrix(self):
         """Prints the rows in the matrix"""
@@ -48,24 +49,24 @@ class Matrix(object):
             print row.values
         print "\n"
 
-# Can't remove len because starting from 1
+    # Can't remove len because starting from 1
     def isRectangular(self):
         """Returns true if matrix is rectangular (all rows of the same length)"""
-        for rowIndex in range(1, len(self.rows)):
-            if row[rowIndex].size != row[rowIndex - 1].size:
+        for row_index in range(1, len(self.rows)):
+            if row[row_index].size != row[row_index - 1].size:
                 return False
         return True
 
-    def arePivotColumnsRepeated(self, pivotDictionary):
+    def arePivotColumnsRepeated(self, pivot_dictionary):
         """Return true if there is another pivot variable in the same column"""
-        for pivotCol in pivotDictionary.keys():
-            if pivotCol == -1:
+        for pivot_col in pivot_dictionary.keys():
+            if pivot_col == -1:
                 continue
-            if len(pivotDictionary[pivotCol]) > 1:
+            if len(pivot_dictionary[pivot_col]) > 1:
                 return True
         return False
 
-# Can't remove len because return index
+    # Can't remove len because return index
     def findLeadingColumn(self, row):
         """Returns the column at which the pivot variable is located"""
         for index in range(0, len(row.values)):
@@ -75,13 +76,13 @@ class Matrix(object):
 
     def findLeadingNumber(self, row):
         """Returns the first non-zero number in a row"""
-        leadingCol = self.findLeadingColumn(row)
-        return row.values[leadingCol]
+        leading_col = self.findLeadingColumn(row)
+        return row.values[leading_col]
 
     def normalize(self):
         """Normalizes all rows in the matrix object"""
         for row in self.rows:
-            if row.isEmpty:
+            if row.empty:
                 continue
             self.normalize_row(row)
 
@@ -98,51 +99,53 @@ class Matrix(object):
 
     def getPivotColDictionary(self):
         """Returns a dictionary of pivot variables along with the column where they are located"""
-        pivotDictionary = defaultdict(list)
+        pivot_dictionary = defaultdict(list)
         for row in self.rows:
-            pivotCol = self.findLeadingColumn(row)
-            pivotDictionary[pivotCol].append(row)
-        return pivotDictionary
+            pivot_col = self.findLeadingColumn(row)
+            pivot_dictionary[pivot_col].append(row)
+        return pivot_dictionary
 
-    def getRowsWithUniquePivots(self, pivotDictionary):
+    # Can't remove len because starting from 1 and comparing different arrays at a time
+    def getRowsWithUniquePivots(self, pivot_dictionary):
         """Returns rows that have a pivot variable in a column with no other pivot variables"""
-        updatedRows = []
-        for pivotCol in pivotDictionary.keys():
-            rowArray = pivotDictionary[pivotCol]
-            mainRow = rowArray[0]
-            updatedRows.append(mainRow)
-            for rowIndex in range(1, len(rowArray)):
-                currentRowValues = rowArray[rowIndex].values
-                updatedRow = []
-                for colIndex in range(0, len(rowArray[rowIndex].values)):
-                    updatedRow.append(currentRowValues[colIndex] - mainRow.values[colIndex])
-                updatedRows.append(Row(updatedRow))
-        return updatedRows
+        updated_rows = []
+        for pivot_col in pivot_dictionary.keys():
+            row_array = pivot_dictionary[pivot_col]
+            main_row = row_array[0]
+            updated_rows.append(main_row)
+            for row_index in range(1, len(row_array)):
+                currentRowValues = row_array[row_index].values
+                updated_row = []
+                for col_index in range(0, len(row_array[row_index].values)):
+                    updated_row.append(currentRowValues[col_index] - main_row.values[col_index])
+                updated_rows.append(Row(updated_row))
+        return updated_rows
 
+    # Can't remove len because starting from 1
     def fromRowEchelonToReducedRowEchelon(self):
         """Converts matrix from row echelon form to reduced row echelon form"""
-        for rowIndex in range(1,len(self.rows)):
-            if self.rows[rowIndex].isEmpty:
+        for row_index in range(1,len(self.rows)):
+            if self.rows[row_index].empty:
                 continue
-            leadingCol = self.findLeadingColumn(self.rows[rowIndex])
-            for i in range((rowIndex - 1),-1,-1):
-                if self.rows[i].values[leadingCol] != 0:
-                    self.rows[i] = self.zerofy(self.rows[i], self.rows[rowIndex], leadingCol)
+            leading_col = self.findLeadingColumn(self.rows[row_index])
+            for i in range((row_index - 1),-1,-1):
+                if self.rows[i].values[leading_col] != 0:
+                    self.rows[i] = self.zerofy(self.rows[i], self.rows[row_index], leading_col)
 
-    def zerofy(self, targetRow, toolRow, targetCol):
-        """to-do"""
-        targetRowValues = targetRow.values
-        toolRowValues = toolRow.values
-        zerofiedRow = []
-        factor = targetRowValues[targetCol]
-        for col in range(0, len(targetRowValues)):
-            zerofiedRow.append(round(float(targetRowValues[col]) - (float(factor) * toolRowValues[col]), 5))
-        return Row(zerofiedRow)
+    # Can't remove len because using two arrays at a time
+    def zerofy(self, target_row, tool_row, targetCol):
+        """It will subtract the target_row from the tool_row in an attempt to create 0's and different pivot columns"""
+        target_row_values = target_row.values
+        tool_row_values = tool_row.values
+        zerofied_row = []
+        factor = target_row_values[targetCol]
+        for col in range(0, len(target_row_values)):
+            zerofied_row.append(round(float(target_row_values[col]) - (float(factor) * tool_row_values[col]), 5))
+        return Row(zerofied_row)
 
 def printSpacers():
     """Prints three empty lines"""
-    for i in range(0,3):
-        print " "
+    print("\n\n\n")
 
 def isValidRowString(rowString):
     """Return if a String is a valid row input of a matrix (ex: '4,3,4,5'). The input 'cat' would return false"""
